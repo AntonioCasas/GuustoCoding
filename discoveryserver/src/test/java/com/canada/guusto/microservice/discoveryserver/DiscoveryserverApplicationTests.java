aa22b837c08edda849caf8d12a0c64c6de2e9552
AntonioCasas <69268539+AntonioCasas@users.noreply.github.com>
2021-12-15 09:16:08 -0800

    Add files via upload
    Discovery Server

---
+++ discoveryserver/src/test/java/com/canada/guusto/microservice/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canada.guusto.microservice.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
