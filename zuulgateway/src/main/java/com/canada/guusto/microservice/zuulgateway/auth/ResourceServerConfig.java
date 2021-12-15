7f787548c472d643d5f39711767073fd8012642c
AntonioCasas <69268539+AntonioCasas@users.noreply.github.com>
2021-12-15 09:17:45 -0800

    Add files via upload
    Zuul Server

---
+++ zuulgateway/src/main/java/com/canada/guusto/microservice/zuulgateway/auth/ResourceServerConfig.java
@@ -0,0 +1,41 @@
+package com.canada.guusto.microservice.zuulgateway.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore());
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/api/oauth/token").permitAll()
+		.anyRequest().authenticated();
+		//.antMatchers("/api/merchants/merchant","/api/merchants/merchant/{id}","/api/users/user/{username}").authenticated()
+		
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("abcde1234");
+		return jwtAccessTokenConverter;
+	}
+
+}
